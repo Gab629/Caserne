db4d956079601e87041f60cde33991873b36c1f6
Gabriel Gauthier <70355153+Gab629@users.noreply.github.com>
2025-04-01 15:19:16 -0400

    Première connexion à la base de donné via VSCode. On peut afficher les données du secteur

---
+++ TestCli.java
@@ -0,0 +1,32 @@
+import java.sql.SQLException;
+
+public class TestCli {
+	public static DbConnection dbConnection;
+    public static void main(String[] args) {
+        try {
+        	//openConnection
+        	DbConnection dbConnection = DbConnection.getInstance();
+        	System.out.println("Connexion à la BD ouverte avec Succés");
+        	
+
+        	GestionCaserne.afficherSecteurs(); //Afficher les secteurs
+            
+        	// GestionCaserne.modifierEmploye(6,"Roe", "Jean"); //modifier les infos de l'employé avec id=6
+            
+        	// GestionCaserne.afficherEmployes(); //afficher tous les employés
+            
+        	// GestionCaserne.afficherUnEmploye(1); //afficher employé avec id =2
+            
+        	// GestionCaserne.supprimerEmploye(6); //supprimer employé avec id =6
+
+
+            // fermer la connection à la base données, avant de quitter
+            dbConnection.closeConnection();
+            System.out.println("Connexion à la BD fermée avec Succés");
+        } 
+        catch (SQLException e) { //une gestion simple des erreurs de la BD
+            System.err.println("Erreur de BD: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
